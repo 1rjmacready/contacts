1554e23a714ee2f5845a2849908b08f921d12756
Mark Sage <idaho.coder@gmail.com>
2021-02-23 19:21:39 -0600

    added @RestController

--- src/main/java/com/maccready/contacts/ContactController.java
+++ src/main/java/com/maccready/contacts/ContactController.java
@@ -4,10 +4,12 @@ import com.maccready.contacts.models.Contact;
 import com.maccready.contacts.services.ContactService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RequestMapping("/contacts")
+@RestController
 public class ContactController {
     private final ContactService service;
 
