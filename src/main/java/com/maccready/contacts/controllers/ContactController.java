18b388e5f7cfffdf53330ef2f254e1899b90f0e1
Mark Sage <idaho.coder@gmail.com>
2021-02-23 21:14:25 -0600

    annotate POST method appropriately

--- src/main/java/com/maccready/contacts/controllers/ContactController.java
+++ src/main/java/com/maccready/contacts/controllers/ContactController.java
@@ -3,9 +3,7 @@ package com.maccready.contacts.controllers;
 import com.maccready.contacts.models.Contact;
 import com.maccready.contacts.services.ContactService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
 import java.net.URISyntaxException;
@@ -24,8 +22,8 @@ public class ContactController {
     List<Contact> getAllContacts(){
         return service.getAll();
     }
-
-    public ResponseEntity<Contact> saveContact(Contact contact) throws URISyntaxException {
+    @PostMapping
+    public ResponseEntity<Contact> saveContact(@RequestBody Contact contact) throws URISyntaxException {
         Long id = service.save(contact);
         return ResponseEntity.created(new URI("/persons/" + id)).body(contact);
     }
