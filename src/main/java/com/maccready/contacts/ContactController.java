2979ac764ce835f815f8f614d4c814894877a7bd
Mark Sage <idaho.coder@gmail.com>
2021-02-23 18:51:42 -0600

    controller endpoint and test

---
+++ src/main/java/com/maccready/contacts/ContactController.java
@@ -0,0 +1,22 @@
+package com.maccready.contacts;
+
+import com.maccready.contacts.models.Contact;
+import com.maccready.contacts.services.ContactService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@RequestMapping("/contacts")
+public class ContactController {
+    private ContactService service;
+
+    public ContactController(ContactService service) {
+        this.service = service;
+    }
+
+    @GetMapping
+    List<Contact> getAllContacts(){
+        return service.getAll();
+    }
+}
