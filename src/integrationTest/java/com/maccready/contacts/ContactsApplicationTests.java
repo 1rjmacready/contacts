367ae4707fd6884ec7ba9111be1c2d959cc7c881
Mark Sage <idaho.coder@gmail.com>
2021-03-02 07:35:28 -0600

    add integration test target

--- src/integrationTest/java/com/maccready/contacts/ContactsApplicationTests.java
+++ src/integrationTest/java/com/maccready/contacts/ContactsApplicationTests.java
@@ -1,9 +1,12 @@
 package com.maccready.contacts;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@SpringBootTest
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 class ContactsApplicationTests {
 
 	@Test
