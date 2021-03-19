d15fa48eaa52236dbea886c2ca08d9693d0bd676
Mark Sage <idaho.coder@gmail.com>
2021-03-19 13:51:12 -0500

    Update request mapping to prevent confusion in upcoming test

--- src/main/java/com/maccready/contacts/controllers/EmployeeController.java
+++ src/main/java/com/maccready/contacts/controllers/EmployeeController.java
@@ -11,7 +11,7 @@ import java.net.URI;
 import java.net.URISyntaxException;
 
 @RestController
-@RequestMapping("/employees")
+@RequestMapping("/wageearners")
 public class EmployeeController {
     private final RestTemplate restTemplate;
 
